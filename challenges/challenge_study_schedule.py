48d2103f4f20fb244d67a7c14c64532305fcd925
lana2-2karla <allanakarla1702@gmail.com>
2022-10-17 15:37:08 -0300

    :sparkles: feat: cria função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    qty_students = 0
+    for period in permanence_period:
+        try:
+            if period[0] <= target_time <= period[1]:
+                qty_students += 1
+        except TypeError:
+            return None
+
+    return qty_students
