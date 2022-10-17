5ceb237846bbc4dfb94265511578885fd9dc9d0a
lana2-2karla <allanakarla1702@gmail.com>
2022-10-17 17:57:31 -0300

    :sparkles: feat: cria função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
 def is_anagram(first_string, second_string):
-    """ """
+
+    first = list(first_string.lower())
+    second = list(second_string.lower())
+
+    if len(first) == 0 or len(second) == 0:
+        return False
+
+    if len(first) != len(second):
+        return False
+
+    for letter in first:
+        if letter in second:
+            second.remove(letter)
+        else:
+            return False
+    return True
+    
